e69e3a2608adc0652aee117cea3bc03948131180
mikalep <itstenlayers@gmail.com>
2022-12-23 15:46:22 +0200

    Implement basic Etch-A-Sketch in browser

--- app.js
+++ app.js
@@ -1,31 +1,66 @@
 const grid        = document.getElementById('grid-container');
 const colorPicker = document.getElementById('colorpicker');
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+const smallBtn   = document.getElementById('btn-small');
+const mediumBtn  = document.getElementById('btn-medium');
+const largeBtn   = document.getElementById('btn-large');
+const defaultBtn = document.getElementById('btn-default');
+const resetBtn   = document.getElementById('btn-reset');
 
-    cell.addEventListener('mouseover', () => {
-      cell.style.background = colorPicker.value
-    });
+const SIZE_SMALL   = 8;
+const SIZE_DEFAULT = 16;
+const SIZE_MEDIUM  = 32;
+const SIZE_LARGE   = 64;
+
+smallBtn.addEventListener('click', () => {
+  renderGrid(SIZE_SMALL);
+});
+
+defaultBtn.addEventListener('click', () => {
+  renderGrid(SIZE_DEFAULT);
+});
+
+mediumBtn.addEventListener('click', () => {
+  renderGrid(SIZE_MEDIUM);
+});
+
+largeBtn.addEventListener('click', () => {
+  renderGrid(SIZE_LARGE);
+});
+
+resetBtn.addEventListener('click', () => {
+  resetGrid();
+});
+
+const renderGrid = (size) => {
+  grid.innerHTML = "";
+
+  grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement('div');
+
+      cell.classList.add('cell');
+      cell.style.setProperty('height', `calc(50rem / ${size})`);
+      cell.style.setProperty('width', `calc(50rem / ${size})`);
+      grid.appendChild(cell);
+
+      cell.addEventListener('mouseover', (event) => {
+        event.target.style.background = colorPicker.value;
+      });
+    }
   }
 }
 
-const initResetBtn = () => {
-  const btn = document.getElementById('reset-btn');
+const resetGrid = () => {
   const cells = document.querySelectorAll('.cell');
-  btn.addEventListener('click', () => {
-    for (const cell of cells) {
-      cell.style.background = "";
-    }
-  });
+
+  for (const cell of cells) {
+    cell.style.background = "";
+  }
 }
 
-initResetBtn();
-
-// - Add buttons to choose grid size - Buttons added but they dont work yet
-// - Add color picker to choose paint color
-// - BONUS: Darken color when cursor stays on one cell
-// - BONUS: Make stuff look nice
+window.onload = () => {
+  renderGrid(SIZE_DEFAULT);
+}
